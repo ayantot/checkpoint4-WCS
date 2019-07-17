16f01a80f425212ca38c319833aae5edf86fa6dc
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-07-17 09:31:41 +0200

    Add routing process to all pages

--- front-circus/src/app/app-routing.module.ts
+++ front-circus/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PriceComponent } from './price/price.component';
+import { MediaComponent } from './media/media.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'price', component: PriceComponent, pathMatch:"full"},
+  {path: 'contact', component: ContactComponent, pathMatch:"full"},
+  {path: 'media', component: MediaComponent, pathMatch:"full"},
+  {path: 'homepage', component: HomepageComponent, pathMatch:"full"},
+  {path: '', redirectTo: '/homepage', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
